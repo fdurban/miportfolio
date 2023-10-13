8da49c135f477c217fd8ecb0e6f953cff75cd6d9
Fernando Durban <ferbrizie@gmail.com>
2023-10-13 19:08:50 +0200

    cambiosfer

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,40 +1,63 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useForm, ValidationError } from '@formspree/react';
 
-const ContactPage = () => {
+function ContactPage() {
+    const [state, handleSubmit] = useForm("mbjvqyvn");
+    if (state.succeeded) {
+        return <p>Thanks for joining!</p>;
+    }
     return (
-        <Form
-            action="mailto:ferbrizie@gmail.com"
-            method="post"
-            encType="text/plain"
-        >
-            <h2>Send e-mail to someone@example.com:</h2>
-            <Form.Group controlId="formName">
-                <Form.Label>Name:</Form.Label>
-                <Form.Control type="text" name="name" />
+        <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="name">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                    type="text"
+                    name="name"
+                    id="name"
+                />
+                <ValidationError
+                    prefix="Name"
+                    field="name"
+                    errors={state.errors}
+                />
             </Form.Group>
 
-            <Form.Group controlId="formEmail">
-                <Form.Label>E-mail:</Form.Label>
-                <Form.Control type="text" name="mail" />
+            <Form.Group controlId="email">
+                <Form.Label>Email Address</Form.Label>
+                <Form.Control
+                    type="email"
+                    name="email"
+                    id="email"
+                />
+                <ValidationError
+                    prefix="Email"
+                    field="email"
+                    errors={state.errors}
+                />
             </Form.Group>
 
-            <Form.Group controlId="formComment">
-                <Form.Label>Comment:</Form.Label>
-                <Form.Control as="textarea" name="comment" rows={3} />
+            <Form.Group controlId="message">
+                <Form.Label>Message</Form.Label>
+                <Form.Control
+                    as="textarea"
+                    name="message"
+                    id="message"
+                />
+                <ValidationError
+                    prefix="Message"
+                    field="message"
+                    errors={state.errors}
+                />
             </Form.Group>
 
-            <Button variant="primary" type="submit">
-                Send
-            </Button>
-
-            <Button variant="secondary" type="reset">
-                Reset
+            <Button type="submit" disabled={state.submitting}>
+                Submit
             </Button>
         </Form>
     );
-};
+}
 
 export default ContactPage;
 
